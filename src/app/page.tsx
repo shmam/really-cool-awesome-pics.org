7cce48cb750905bcb9c962753d0ecdbb024e4c3d
shmam <shmam@github.com>
2026-06-25 14:22:36 -0700

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,12 @@ export default async function Home() {
       <h1>really-cool-awesome-pics.org</h1>
       <p>a photo blog by sam crochet, who wants to share some 35mm photos</p>
       <details>
-        <summary>more</summary>
+        <summary>more details</summary>
         <ul>
           <li>image order is shuffed on each page load (for fun)</li>
           <li>photos were taken with either my contax rx (50mm lens) or my konika big mini</li>
           <li>this site is hosted completely (for free) on cloudflare</li>
+          <li>I actually had to implement my own lazy loading component, because safari/webkit was not respecting the native lazy loading attribute</li>
         </ul>
       </details>
       <p>tell me which ones you like: <a href="mailto:samuel.d.crochet@gmail.com">samuel.d.crochet@gmail.com</a></p>
